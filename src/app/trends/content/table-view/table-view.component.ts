2b7dc56403167d72bb9b5e09b3bd17de591c8f72
mrhb <mohammad_hajjar@yahoo.com>
2020-09-16 11:34:21 +0430

    toolbar of table view in trend page added

--- src/app/trends/content/table-view/table-view.component.ts
+++ src/app/trends/content/table-view/table-view.component.ts
@@ -4,13 +4,28 @@ import{trendViewComponent} from '../trendView.Cmponent'
 @Component({
   selector: 'app-table-view',
   templateUrl: './table-view.component.html',
-  styleUrls: ['./table-view.component.sass']
+  styleUrls: ['./table-view.component.css']
 })
 export class TableViewComponent implements trendViewComponent, OnInit {
 
   constructor() { }
   data: SeriesInfo[];
-
+  Ranges = [
+    { value: 1, label: 'Today'},
+    { value: 2, label: 'Last 3 Days'},
+    { value: 2, label: 'Last Week'},
+    { value: 2, label: 'Last Month'},
+    { value: 2, label: 'Last 2 Month'},
+    { value: 2, label: 'Last 3 Month'},
+    { value: 2, label: 'Last 6 Month'},
+    { value: 2, label: 'Last 9 Month'},
+    { value: 2, label: 'Last 1 Year'},
+    { value: 3, label: 'From - To' }
+   ];
+   Tools=[
+     { value: 1, label: 'Reload all data'},
+    { value: 2, label: 'Export data as XLSX'},
+  ];
   ngOnInit(): void {
   }
 
