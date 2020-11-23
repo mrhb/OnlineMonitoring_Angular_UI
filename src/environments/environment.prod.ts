50743749f3233cf404988f22662bf4b3f0cb0de0
mrhb <mohammad_hajjar@yahoo.com>
2020-11-23 08:31:56 +0330

    deployed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   api: 'http://185.252.28.72:3600/api',
+  sidebar: 'http://localhost:3600/mock',
+  apiUrl: 'http://localhost:3600',
   useMockServer: false
 };
