b256c6a68dc891d75baae7f1ff9aea43d55e93b3
mrhb <mohammad_hajjar@yahoo.com>
2020-11-26 08:09:59 +0330

    change ivirement urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3600/api',
+  api: 'http://185.252.28.72:3600/api',
   sidebar: 'http://localhost:3600/mock',
-  authUrl: 'http://localhost:3600',
+  authUrl: 'http://185.252.28.72:3600',
   useMockServer: false
 };
