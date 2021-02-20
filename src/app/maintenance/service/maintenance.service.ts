85ed38d9f869b47c160d779b099f0f4c2dbf37c6
mrhb <mmmhajjar83gmail.com>
2021-02-20 15:30:47 +0330

    maintenance service completed

--- src/app/maintenance/service/maintenance.service.ts
+++ src/app/maintenance/service/maintenance.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { Observable } from 'rxjs';
+
+import { Maintenance,MaintenanceInfo } from './maintenance';
+
+const baseUrl =environment.userUrl+ '/api/maintenance';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MaintenanceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllMaintenances(): Observable<MaintenanceInfo[]> {
+    return this.http.get<MaintenanceInfo[]>(baseUrl);
+  }
+
+  getMaintenance(id): Observable<any> {
+    return this.http.get<MaintenanceInfo>(`${baseUrl}/${id}`);
+  }
+
+  updateMaintenanceById(id, data): Observable<any> {
+    return this.http.patch<MaintenanceInfo>(`${baseUrl}/${id}`, data);
+  }
 }
--- src/app/maintenance/service/maintenance.ts
+++ src/app/maintenance/service/maintenance.ts
@@ -1,1 +1,16 @@
-// maintenance
+export interface MaintenanceInfo {
+    id:number;
+    unitName:string;
+    maintenances:Maintenance[];
+}
+
+export interface Maintenance {
+    name: string;
+    criteria:{
+        type: String,
+        enum: ['day','runHour']
+    },
+    duration:Number;
+    runHour:Number;
+    date:Number;
+}
