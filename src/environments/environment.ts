b2cf1ce1923c995daeb7ef24bdfb2c5b833a9107
mrhb <mohammad_hajjar@yahoo.com>
2021-03-03 15:26:46 +0330

    paginator به صفحه کاربران و ژنراتورها افزوده شد.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: true,
-// api:     'http://185.252.28.72:3600/api',
-   api:     'http://localhost:3600/api',
-   sidebar: 'http://localhost:3600/api',
-   authUrl: 'http://localhost:3600',
-   userUrl: 'http://localhost:3600',
-   useMockServer: false
+  api: 'http://185.252.28.72:3600/api',
+  sidebar: 'http://185.252.28.72:3600/api',
+  authUrl: 'http://185.252.28.72:3600',
+  userUrl: 'http://185.252.28.72:3600',
+  useMockServer: false
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
