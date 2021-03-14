18aace9c416aa0c94a0871d30e365a6917e61eb5
mrhb <mohammad_hajjar@yahoo.com>
2021-03-14 13:08:37 +0330

    colors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  api: 'http://185.252.28.72:3600/api',
-  sidebar: 'http://185.252.28.72:3600/api',
-  authUrl: 'http://185.252.28.72:3600',
-  userUrl: 'http://185.252.28.72:3600',
+  api: 'http://tetavisor.com/api',
+  sidebar: 'http://tetavisor.com/api',
+  authUrl: 'http://tetavisor.com',
+  userUrl: 'http://tetavisor.com',
   useMockServer: false
 };
